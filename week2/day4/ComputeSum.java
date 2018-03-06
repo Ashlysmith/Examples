dd2b291f96e929c934d627c473d146c2914c81cb
Alina Makei <31908390+Ashlysmith@users.noreply.github.com>
2018-03-05 16:43:27 -0800

    Create ComputeSum.java

---
+++ week2/day4/ComputeSum.java
@@ -0,0 +1,18 @@
+package week2.day4.appcode;
+
+public class ComputeSum {
+	
+	public static int sumNumber(int a, int b) {
+		int result = a + b;
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		int b = 2;
+		System.out.println(sumNumber(a,b));
+
+	}
+
+}
